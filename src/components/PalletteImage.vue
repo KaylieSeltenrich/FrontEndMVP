3fe3f2dea44fbff0a7268cd645ff176023b1b803
orcinusorca1995 <kaylie.seltenrich@gmail.com>
2020-12-08 20:36:03 -0700

    Lots of changes to frontend to get colourpicker and image search to work, started to make signup and login forms

---
+++ src/components/PalletteImage.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <img
+      class="image"
+      @click="GetPallette($event.currentTarget)"
+      v-bind:src="image.src.medium"
+      width="500px"
+    />
+    <div class="pallettecolor" v-for="color in colors" :key="color">
+      <div class="fill" v-bind:style="'background-color:'+ color"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ColorThief from "colorthief";
+
+export default {
+  props: {
+    image: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      colorThief: new ColorThief(),
+      colors: [],
+    };
+  },
+  methods: {
+    RGBToHex: function(r, g, b) {
+      r = r.toString(16);
+      g = g.toString(16);
+      b = b.toString(16);
+
+      if (r.length == 1) r = "0" + r;
+      if (g.length == 1) g = "0" + g;
+      if (b.length == 1) b = "0" + b;
+
+      return "#" + r + g + b;
+    },
+
+    GetPallette: function(image) {
+  
+     image.crossOrigin = 'Anonymous';
+      var color = this.colorThief.getPalette(image);
+      var i = 0;
+      this.colors = [];
+      for (i = 0; i < 10; i++) {
+        var hex = this.RGBToHex(color[i][0], color[i][1], color[i][2]);
+        this.colors.push(hex);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.pallettecolor {
+  display: inline-block;
+  width: 5vw;
+  height: 10vh;
+}
+.fill {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/components/UserSignup.vue
+++ src/components/UserSignup.vue
@@ -7,13 +7,53 @@
       <input type="text" v-model="email" />
       <p>Password:</p>
       <input type="password" v-model="password" />
-      <h2 @click="createUser">Sign Up</h2>
+      <h2 @click="signupUser()">Sign Up</h2>
     </div>
   </div>
 </template>
 
 <script>
 
+import axios from "axios";
+import cookies from "vue-cookies";
+
+export default {
+  name: "signup-form",
+  data() {
+    return {
+      username: "",
+      password: "",
+      email: "",
+    };
+  },
+
+  methods: {
+    signupUser: function() {
+      axios
+        .request({
+          url: "http://127.0.0.1:5000/api/users",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "X-Api-Key": "SVzuhkqP5JrStTsfETYXW6UQZs0UV95ENy1VscJoZ3L5P"
+          },
+          data: {
+            email: this.email,
+            username: this.username,
+            password: this.password,
+          }
+        })
+        .then(response => {
+          console.log(response.data);
+          cookies.set("session", response.data[0].loginToken);
+          this.$router.push("/home");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
 </script>
 
 <style lang="css" scoped>
